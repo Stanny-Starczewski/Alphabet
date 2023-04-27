eba86de0b4a3b5ee33b9829f556da536e7ee75de
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-04-26 20:06:01 -0400

    Initial Commit

---
+++ Alphabet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alphabet
+//
+//  Created by Gennadii Kulikov on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
