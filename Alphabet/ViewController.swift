f2064668ebbf0b62060fd03310c9ea88cb1ed469
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-04-27 16:56:44 -0400

    Спринт 14/18: 14 → Тема 2/4: Коллекции → Урок 4/5

--- Alphabet/ViewController.swift
+++ Alphabet/ViewController.swift
@@ -15,8 +15,10 @@ class ViewController: UIViewController {
         cofigueView()
         configueConstraints()
         collectionView.dataSource = self
-        //collectionView.delegate = self
+        collectionView.delegate = self
         collectionView.register(LetterCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(SupplementaryView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
+        collectionView.register(SupplementaryView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "footer")
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.collectionViewLayout = UICollectionViewFlowLayout()
     }
@@ -48,11 +50,29 @@ extension ViewController: UICollectionViewDataSource {
         return cell!
     }
     //сама ячейка для заданной позиции IndexPath
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        var id: String
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            id = "header"
+        case UICollectionView.elementKindSectionFooter:
+            id = "footer"
+        default:
+            id = ""
+        }
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: id, for: indexPath) as! SupplementaryView
+        view.titleLabel.text = "Здесь назодится Supplementary View"
+        return view
+    }
+    //возвращать хедер и футер для каждой секции
 }
 
-/*
- //не понимаю, зачем нам нужен был делегат?
-extension ViewController: UICollectionViewDelegate {
+
+ //не понимаю, зачем нам нужен был этот делегат?
+//extension ViewController: UICollectionViewDelegate {
+    
+    /*
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
     }
@@ -67,6 +87,37 @@ extension ViewController: UICollectionViewDelegate {
         
     }
     //задаёт минимальный отступ между ячейками
+    */
+    
+//}
+extension ViewController: UICollectionViewDelegateFlowLayout { // 1 Для управления расположением и размерами элементов (включая размеры хедера) нужно реализовать методы из протокола
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize { // 2 реализуем метод, получает на вход объект коллекции, layout и номер секции, а возвращает её (секции) размер.
+        
+        let indexPath = IndexPath(row: 0, section: section) // 3 Так как мы получили только номер секции, теперь нам нужно самим собрать структуру IndexPath для дальнейшего использования
+        let headerView = self.collectionView(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: indexPath) // 4 Получаем у коллекции View — так же, как мы делали это для ячеек. Но на этот раз используем немного другой метод
+        
+        return headerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width,
+                                                         height: UIView.layoutFittingExpandedSize.height),
+                                                         withHorizontalFittingPriority: .required,
+                                                         verticalFittingPriority: .fittingSizeLevel)// 5 передаём View возможность посчитать свой размер и вернуть его. Зная текст, мы могли бы рассчитать высоту View, но такой подход более универсален: он фиксирует ширину View по размеру коллекции, но даёт ей рассчитать свою высоту самостоятельно исходя из констрейнтов. Если вы заглянете в код SupplementaryView, то увидите, что мы задали лейблу верхний и нижний констрейнты к самой View. От размеров лейбла и будет зависеть высота всей View
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        let indexPath = IndexPath(row: 0, section: section)
+        let footerView = self.collectionView(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionFooter, at: indexPath)
+        
+        return footerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width,
+                                                         height: UIView.layoutFittingExpandedSize.height),
+                                                  withHorizontalFittingPriority: .required,
+                                                  verticalFittingPriority: .fittingSizeLevel)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize { // 1 Мы добавляем реализацию метода
+        return CGSize(width: collectionView.bounds.width / 2, height: 50)   // 2 Берём ширину коллекции и делим на два, чтобы на каждой строке помещалось ровно по две ячейки. Высоту выставляем
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
     
 }
-*/
